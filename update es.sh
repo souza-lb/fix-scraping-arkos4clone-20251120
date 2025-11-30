91215341e0dacd56546b3afb0b9aed7ca7c8193e
souza-lb <162514139+souza-lb@users.noreply.github.com>
2025-11-30 10:35:46 -0300

    Add files via upload

---
+++ update es.sh
@@ -0,0 +1,4 @@
+sudo cp /roms/ports/emulationstation /usr/bin/emulationstation/emulationstation
+sudo chmod 777 /usr/bin/emulationstation/emulationstation
+sudo rm /roms/ports/emulationstation
+sudo rm -- "$0"
